87f35c2ca90c73685fc0992fb0f5bf9c20e985c9
Rakibul Hasan <rakibulhasan3929@gmail.com>
2026-02-03 08:36:59 +0600

    updated code

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -5,8 +5,11 @@ import { UserRoutes } from "../modules/user/user.route";
 
 const router = Router();
 
+// user routes
 router.use("/users", UserRoutes);
+// auth routes;
 router.use("/auth", AuthRoutes);
+// invitation routes
 router.use("/invite", InviteRoutes);
 
 export default router;
