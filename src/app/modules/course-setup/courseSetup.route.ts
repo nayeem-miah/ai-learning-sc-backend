18ca97e689203f41eed65e9e347e608c37e6df32
Rakibul Hasan <rakibulhasan3929@gmail.com>
2026-03-11 09:09:29 +0600

    routing editing

--- src/app/modules/course-setup/courseSetup.route.ts
+++ src/app/modules/course-setup/courseSetup.route.ts
@@ -1,14 +1,14 @@
-import { Router } from 'express';
-import auth from '../../middlewares/auth';
-import { CourseSetupController } from './courseSetup.controller';
+import { Router } from "express";
+import auth from "../../middlewares/auth";
+import { CourseSetupController } from "./courseSetup.controller";
 
 const router = Router();
+// const router = express.Router();
 
-router.post('/setup', auth('ADMIN'), CourseSetupController.courseSetup);
+router.post("/setup", auth("ADMIN"), CourseSetupController.courseSetup);
 
-router.get('/all', CourseSetupController.getAllCourses);
+router.get("/all", CourseSetupController.getAllCourses);
 
-router.get('/:session_id', CourseSetupController.getCourseBySession);
+router.get("/:session_id", CourseSetupController.getCourseBySession);
 
 export const CourseSetupRoutes = router;
-
