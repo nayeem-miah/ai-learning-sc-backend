38f488c6ff9fac646217fb7d7afbef67b457237d
nayeem-miah <nayeem5113a@gmail.com>
2026-03-12 13:22:41 +0600

    get all teacher

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -1,59 +1,62 @@
-import express from "express";
-import validateRequest from "../../middlewares/validateRequest";
+import express from 'express';
+import validateRequest from '../../middlewares/validateRequest';
 
-import { Role } from "@prisma/client";
-import auth from "../../middlewares/auth";
-import { fileUpload } from "../../utils/fileUpload";
-import { UserController } from "./user.controller";
-import { userValidation } from "./user.validation";
+import { Role } from '@prisma/client';
+import auth from '../../middlewares/auth';
+import { fileUpload } from '../../utils/fileUpload';
+import { UserController } from './user.controller';
+import { userValidation } from './user.validation';
 
 const router = express.Router();
 
 // Existing routes
 router.post(
-  "/student",
+  '/student',
   validateRequest(userValidation.createStudentValidationSchema),
   UserController.registerStudent,
 );
 router.patch(
-  "/update-profile",
-  fileUpload.upload.single("file"),
+  '/update-profile',
+  fileUpload.upload.single('file'),
   auth(Role.STUDENT, Role.ADMIN),
   UserController.updateProfile,
 );
 
 router.post(
-  "/linked-users",
+  '/linked-users',
   auth(Role.STUDENT, Role.ADMIN),
   // validateRequest(userValidation.createUserLinkSchema),
-  UserController.createUserLink
+  UserController.createUserLink,
 );
 
 router.get(
-  "/linked-users",
+  '/linked-users',
   auth(Role.STUDENT, Role.ADMIN),
-  UserController.getLinkedUsers
+  UserController.getLinkedUsers,
 );
 
 router.delete(
-  "/linked-users/:targetUserId",
+  '/linked-users/:targetUserId',
   auth(Role.STUDENT, Role.ADMIN),
-  // validateRequest(userValidation.removeUserLinkSchema),
-  UserController.removeUserLink
+  UserController.removeUserLink,
 );
 
 // Student CRUD
-router.get("/", auth(), UserController.getAllStudents);
-router.get("/teachers", auth(Role.ADMIN, Role.STUDENT, Role.TEACHER), UserController.getAllTeachers);
-router.get("/me", auth(), UserController.getMyProfile);
-router.get("/:id", auth(), UserController.getStudentById);
+router.get('/', auth(), UserController.getAllStudents);
+router.get(
+  '/teachers',
+  auth(Role.ADMIN, Role.STUDENT, Role.TEACHER),
+  UserController.getAllTeachers,
+);
+router.get('/me', auth(), UserController.getMyProfile);
+router.get('/:id', auth(), UserController.getStudentById);
 
 // admin access share users
-router.patch("/:id", auth(Role.ADMIN), UserController.toggleUserRole);
+router.patch('/:id', auth(Role.ADMIN), UserController.toggleUserRole);
 
 // admin delete user account
-router.delete("/:id", UserController.deleteStudent);
+router.delete('/:id', UserController.deleteStudent);
 // user delete his account
-router.delete("/me/delete", auth(), UserController.deleteMe);
+router.delete('/me/delete', auth(), UserController.deleteMe);
 
 export const UserRoutes = router;
